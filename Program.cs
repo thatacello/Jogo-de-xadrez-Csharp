73faf88876f0a825bd37d247d6cc2fa1cc002fa4
tsci <tsci@gft.com>
2021-05-13 16:45:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jogo_de_xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
